979251ff005686252309cd9b427013ebf751b7e5
zeno-citiulo <zenocit02@gmail.com>
2024-12-02 21:19:43 +0100

    feat: snack-5 completed

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,6 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+const evenNums = nums.filter(num => num % 2 === 0); console.log(evenNums);
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
