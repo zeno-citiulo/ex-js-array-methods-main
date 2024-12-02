a87a4358571dd44b78b607e7eb3f720720750857
zeno-citiulo <zenocit02@gmail.com>
2024-12-02 21:17:40 +0100

    feat: exercise snack-4 completed

--- snack-4/script.js
+++ snack-4/script.js
@@ -5,5 +5,8 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+const authors = posts.map(post => post.author);
+
+console.log(authors);
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
